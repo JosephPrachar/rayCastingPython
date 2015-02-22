1b465734084207357a74227dfa2a42ede2475c33
Joseph Prachar <josephprachar@comcast.net>
2015-02-21 19:06:11 -0800

    Refactored code plus timed execution.

--- casting_test.py
+++ casting_test.py
@@ -1,13 +1,18 @@
 import data
 import cast
+import sys
+import datetime
+
 
 def main():
+    start_time = datetime.datetime.now()
+
     # create scene
     eye = data.Point(0.0, 0.0, -14.0)
     spheres = [data.Sphere(data.Point(.5, 1.5, -3.0), 0.5, data.Color(1, 0, 0), data.Finish(.4, .4, .5, .05)),
                data.Sphere(data.Point(1.0, 1.0, 0.0), 2.0, data.Color(0, 0, 1), data.Finish(.2, .4, .5, .05))]
-    ambientColor = data.Color(1.0,1.0,1.0)
-    pointLight = data.Light(data.Point(-100.0, 100.0, -100.0), data.Color(1.5,1.5,1.5))
+    ambientColor = data.Color(1.0, 1.0, 1.0)
+    pointLight = data.Light(data.Point(-100.0, 100.0, -100.0), data.Color(1.5, 1.5, 1.5))
 
     # easy scaling of final image size
     scale = 1
@@ -20,6 +25,11 @@ def main():
     print 255
 
     cast.cast_all_rays(-10, 10, -7.5, 7.5, width, height, eye, spheres, ambientColor, pointLight)
+
+    end_time = datetime.datetime.now()
+    diff_time = end_time - start_time
+    print >> sys.stderr, diff_time
+
 
 if __name__ == "__main__":
     main()
--- funcs.py
+++ funcs.py
@@ -1,13 +1,17 @@
 import math
 
+
 def f(x):
-  return 7*(x**2) + 2*x
+    return 7 * (x ** 2) + 2 * x
+
 
 def g(x, y):
-  return x**2 + y**2
+    return x ** 2 + y ** 2
+
 
 def hypotenuse(a, b):
-  return math.sqrt(g(a, b))
+    return math.sqrt(g(a, b))
+
 
 def is_positive(x):
-  return x >= 0
+    return x >= 0
