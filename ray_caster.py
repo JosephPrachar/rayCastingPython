3af74e4ef9648056b9e1d6440ddc73afccd1bbb4
Joseph Prachar <josephprachar@comcast.net>
2015-02-25 23:08:07 -0800

    Minor changes

--- ray_caster.py
+++ ray_caster.py
@@ -4,7 +4,6 @@ import cast
 import datetime
 
 def main(argv):
-    # todo: take comandline input
     args = commandline.process_args(argv)
     # tuple
     # 0: sphere list
@@ -17,7 +16,7 @@ def main(argv):
 
     with open('image.ppm', 'w') as output_file:
         output_file.write('P3\n' + str(args[2][4]) + ' ' + str(args[2][5]) + '\n255\n')
-        #print 'P3\n' + str(args[2][4]) + ' ' + str(args[2][5]) + '\n255'
+
         cast.cast_all_rays(args[2][0], args[2][1], args[2][2], args[2][3], args[2][4], args[2][5],
                            args[1], args[0], args[4], args[3], output_file)
 
